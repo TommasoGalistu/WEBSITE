bbcc51023dda2fd2d07fab2168c70f84e4c1fcb2
TommasoGalistu <165350030+TommasoGalistu@users.noreply.github.com>
2024-09-19 13:13:02 +0200

    fix:real send email

--- src/components/page/ContactPage.vue
+++ src/components/page/ContactPage.vue
@@ -1,10 +1,50 @@
 <script>
+import emailjs from "emailjs-com";
 export default {
   name: "ContactPage",
   data() {
-    return {};
+    return {
+      form: {
+        name: "",
+        email: "",
+        subject: "",
+        message: "",
+      },
+      emailSent: false,
+    };
   },
-  methods: {},
+  methods: {
+    sendEmail() {
+      const serviceID = "service_3gm2suh"; // Sostituisci con il tuo Service ID da EmailJS
+      const templateID = "template_pjqfm3p"; // Sostituisci con il tuo Template ID
+      const userID = "4PLSrtVRdufkeuRQ8"; // Sostituisci con il tuo User ID
+
+      const templateParams = {
+        from_name: this.form.name,
+        from_email: this.form.email,
+        subject: this.form.subject,
+        message: this.form.message,
+      };
+
+      emailjs
+        .send(serviceID, templateID, templateParams, userID)
+        .then(() => {
+          this.emailSent = true;
+          this.resetForm();
+        })
+        .catch((error) => {
+          console.error("Errore nell'invio dell'email: ", error);
+        });
+    },
+    resetForm() {
+      this.form = {
+        name: "",
+        email: "",
+        subject: "",
+        message: "",
+      };
+    },
+  },
 };
 </script>
 <template>
@@ -26,18 +66,24 @@ export default {
         <div class="tcard text-center formContainer">
           <span class="importantText"> Contattami</span>
 
-          <form class="mt-4" action="">
-            <input type="text" placeholder="Name*" />
-            <input type="email" placeholder="Email*" />
-            <input type="text" placeholder="Oggetto*" />
-            <textarea
-              name=""
-              id=""
-              cols="30"
-              rows="10"
-              placeholder="Il tuo messaggio"
-            ></textarea>
-            <button type="submit">invia</button>
+          <form @submit.prevent="sendEmail">
+            <div>
+              <label for="name">Nome:</label>
+              <input type="text" v-model="form.name" id="name" required />
+            </div>
+            <div>
+              <label for="email">Email:</label>
+              <input type="email" v-model="form.email" id="email" required />
+            </div>
+            <div>
+              <label for="subject">Oggetto:</label>
+              <input type="text" v-model="form.subject" id="subject" required />
+            </div>
+            <div>
+              <label for="message">Messaggio:</label>
+              <textarea v-model="form.message" id="message" required></textarea>
+            </div>
+            <button type="submit">Invia</button>
           </form>
         </div>
       </div>
