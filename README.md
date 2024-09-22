7090d876d1aa5e670af5515207dfa7e45610c88c
TommasoGalistu <165350030+TommasoGalistu@users.noreply.github.com>
2024-09-22 18:44:32 +0200

    fix: problem in pull request

--- README.md
+++ README.md
@@ -2,7 +2,6 @@ cose da fare
 
 fare il banner in home per ultimo progetto
 
-<<<<<<< HEAD
 sistemare h4 nei progetti principali al break 992
 
 sistemare lista tecnologia dentro ogni proj
@@ -14,8 +13,3 @@ link bottoni nei contatti
 break 767 ad ogni proj mettere i bottoni anche sotto la descrizione
 
 margine bottom nel contatti sotto email e link break 767
-=======
-finire il footer
-
-fare le card dei vari project responsive
->>>>>>> 2eabf34826fb59ae1db0c04478d7c23497c17675
--- src/components/page/ProjectOne.vue
+++ src/components/page/ProjectOne.vue
@@ -13,39 +13,13 @@ export default {
   //   props: {
   //     index: Number,
   //   },
-<<<<<<< HEAD
   methods: {},
-=======
-  methods: {
-    // changeSticky() {
-    //   // fermo il contenitore prima de footer
-    //   const stickyElement = this.$refs.stickyElement;
-    //   const container = this.$refs.cont;
-    //   const containerRect = container.getBoundingClientRect();
-    //   const stickyRect = stickyElement.getBoundingClientRect();
-    //   const stickyHeight = stickyElement.offsetHeight;
-    //   const windowHeight = window.innerHeight;
-    //   const containerBottom = containerRect.bottom;
-    //   // Verifica se l'elemento sticky ha raggiunto il fondo del contenitore
-    //   if (containerBottom <= windowHeight - stickyHeight) {
-    //     // Usa transform per "fermare" lo sticky all'interno del contenitore
-    //     stickyElement.style.transform = `translateY(${
-    //       containerBottom - windowHeight - stickyHeight
-    //     }px)`;
-    //   } else {
-    //     // Reset transform per mantenere lo sticky normale
-    //     stickyElement.style.transform = "none";
-    //   }
-    // },
-  },
->>>>>>> 2eabf34826fb59ae1db0c04478d7c23497c17675
   created() {
     // this.projectId = this.$route.params.id;
     this.card = data.cardProject[this.$route.params.id];
   },
   mounted() {
     // Crea un nuovo controller ScrollMagic
-<<<<<<< HEAD
     // const controller = new ScrollMagic.Controller();
     // const pageHeight = document.documentElement.scrollHeight;
     // new ScrollMagic.Scene({
@@ -65,41 +39,13 @@ export default {
     //   })
     //   .addTo(controller);
     // window.addEventListener("scroll", this.changeSticky());
-=======
-    const controller = new ScrollMagic.Controller();
-    const pageHeight = document.documentElement.scrollHeight;
-    new ScrollMagic.Scene({
-      triggerElement: this.$refs.pinElement,
-      triggerHook: 0.05, // Pinnato prima che raggiunga il top della pagina
-      duration: pageHeight * 0.23,
-    })
-      .setPin(this.$refs.pinElement, { pushFollowers: true })
-      .on("enter", (event) => {
-        const spacer = document.querySelector(".scrollmagic-pin-spacer");
-        spacer.style.width = "calc((100% / 12) * 4)";
-        spacer.style.height = "500px";
-        spacer.style.margin = "0";
-        spacer.style.padding = "0";
-        this.$refs.pinElement.style.transition =
-          "transform 0.3s ease, top 0.3s ease";
-      })
-      .addTo(controller);
-
-    // window.addEventListener("scroll", this.changeSticky);
->>>>>>> 2eabf34826fb59ae1db0c04478d7c23497c17675
   },
 };
 </script>
 <template>
-<<<<<<< HEAD
   <div class="container" ref="container">
     <div class="row">
       <div class="col-md-4 foto" ref="stickyElement">
-=======
-  <div class="container" ref="cont">
-    <div class="trow">
-      <div class="tcol foto pin-spacer" ref="pinElement">
->>>>>>> 2eabf34826fb59ae1db0c04478d7c23497c17675
         <div
           class="tcard d-flex flex-column justify-content-center align-items-center gap-3"
         >
@@ -112,11 +58,7 @@ export default {
           <a :href="card.linkGit" class="button">GitHub</a>
         </div>
       </div>
-<<<<<<< HEAD
       <div class="col-md-8 colDestra">
-=======
-      <div class="tcol colDestra">
->>>>>>> 2eabf34826fb59ae1db0c04478d7c23497c17675
         <div class="tcard">
           <div class="contDescription">
             <h3 class="importantText">Descrizione del progetto</h3>
@@ -161,19 +103,11 @@ export default {
   }
 }
 @media screen and (min-width: 768px) {
-<<<<<<< HEAD
   // .foto {
   //   height: 6rem;
   //   position: sticky;
   //   top: 0;
   // }
-=======
-  .foto {
-    height: 6rem;
-    position: sticky;
-    top: 0;
-  }
->>>>>>> 2eabf34826fb59ae1db0c04478d7c23497c17675
 
   .foto.active {
   }
@@ -204,7 +138,6 @@ export default {
   border-radius: 50%;
   border: 1px solid $colorSite;
 }
-<<<<<<< HEAD
 // .pin-spacer {
 //   width: calc((100% / 12) * 4);
 //   position: relative; /* Evita sovrapposizioni */
@@ -225,26 +158,4 @@ export default {
 // .scrollmagic-pin-spacer {
 //   transition: all 0.3s ease;
 // }
-=======
-.pin-spacer {
-  width: calc((100% / 12) * 4);
-  position: relative; /* Evita sovrapposizioni */
-  margin: 0;
-  padding: 0;
-  transition: transform 0.3s ease, top 0.3s ease;
-  transition: top 0.3s ease; /* Imposta una transizione dolce per la posizione */
-}
-.trow {
-  display: flex;
-  margin: 0;
-  padding: 0;
-  gap: 2rem;
-}
-.colDestra {
-  width: calc((100% / 12) * 8);
-}
-.scrollmagic-pin-spacer {
-  transition: all 0.3s ease;
-}
->>>>>>> 2eabf34826fb59ae1db0c04478d7c23497c17675
 </style>
