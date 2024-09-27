10072812c86edb498bb281a45bbb5a85ba4ebbf1
TommasoGalistu <165350030+TommasoGalistu@users.noreply.github.com>
2024-09-27 11:19:30 +0200

    fix:vite config

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  // server: {
-  //   host: "0.0.0.0", // Accetta connessioni da qualsiasi IP
-  //   port: 3000, // Puoi cambiare la porta se vuoi
-  // },
+  
 });
